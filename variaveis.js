01d0fd5baf2c93a35b3ede3477dbefbe0b8b13a1
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-02-26 20:01:09 -0300

    inical code

---
+++ variaveis.js
@@ -0,0 +1,10 @@
+// condicional
+
+const idade = 29 
+
+if (idade<30){
+    console.log(`para`)
+}
+else{
+    console.log(`para2`)
+}
---
+++ variaveis2.js
@@ -0,0 +1,6 @@
+let nome = 'Marta'
+let idade = 20
+
+console.log('A paciente', nome, 'tem', idade,'anos.')
+
+console.log(`A paciente ${nome} tem ${idade} anos.`)
