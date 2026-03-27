56ec349afa403cb66ee52e124ac6e0d8682b8b49
niltonsSantana-devBa <nilton.s.santana@ba.estudante.senai.br>
2026-03-26 21:19:02 -0300

    react

---
+++ primeiro-projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+
+export default function Mensagem(){
+    return(
+        <div className='mensagem'>
+            <h1>Component</h1>
+            <p>Este é um Componente simples.</p>
+        </div>
+    )
+}
